e350de29af6f4a0eea1a15da4bf16bb497d62543
ozgesemiz <ozgesemiz@hotmail.co.uk>
2026-03-05 14:12:11 +0300

    conform script v1.0

--- conform.py
+++ conform.py
@@ -3,7 +3,7 @@
 #Input is a vector of F's and B's, in terms of forwards and backwards caps
 #Output is a set of commands (printed out) to get either all F's or all B's
 #Fewest commands are the goal
-
+# This script is written by Özge Semiz
 caps = ['F', 'F', 'B', 'B', 'B', 'F', 'B',
         'B', 'B', 'F', 'F', 'B', 'F' ]
 cap2 = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'F', 'F' ]
